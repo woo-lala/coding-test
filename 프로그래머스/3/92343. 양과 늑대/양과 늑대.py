9d4b70884a8e924aed83b01b613448b14308337a
WOOLALA <74398314+woo-lala@users.noreply.github.com>
2025-09-26 07:23:27 +0900

    [level 3] Title: 양과 늑대, Time: 10.67 ms, Memory: 9.41 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
@@ -2,7 +2,7 @@ def solution(info, edges):
     visited = [False] * len(info)
     
     cnt_wolf = 0
-    cnt_sheep = 0
+    cnt_sheep = 1 #문제에 항상 시작인 1이라고 나와있음
     
     possible = list()
     
@@ -20,11 +20,6 @@ def solution(info, edges):
                 else:
                     backtrack(cnt_sheep, cnt_wolf + 1)
                 visited[e] = False
-                    
-    if not info[0]:
-        cnt_sheep += 1
-    else:
-        cnt_wolf += 1
     
     visited[0] = True
     backtrack(cnt_sheep, cnt_wolf)
