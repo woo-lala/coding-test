36a85ba15d66a87a14ba6667cf99e0bf791d9ee7
WOOLALA <74398314+woo-lala@users.noreply.github.com>
2025-10-05 09:22:04 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -4,3 +4,21 @@ JOIN ECOLI_DATA e2 ON e2.parent_id = e1.id -- 2세대
 JOIN ECOLI_DATA e3 ON e3.parent_id = e2.id -- 3세대
 WHERE e1.parent_id IS NULL
 ORDER BY e3.id;
+
+# WITH RECURSIVE Generation AS (
+#     -- 앵커 파트: 1세대 개체
+#     SELECT ID, PARENT_ID, 1 AS GENERATION
+#     FROM ECOLI_DATA
+#     WHERE PARENT_ID IS NULL
+
+#     UNION ALL
+
+#     -- 재귀 파트: 다음 세대 개체 찾기
+#     SELECT E.ID, E.PARENT_ID, G.GENERATION + 1
+#     FROM ECOLI_DATA AS E
+#     JOIN Generation AS G ON E.PARENT_ID = G.ID
+# )
+# SELECT ID
+# FROM Generation
+# WHERE GENERATION = 3
+# ORDER BY ID ASC;
